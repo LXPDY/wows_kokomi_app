77a0d6b17400686d51b0a1096b10d55cf4ba6a94
zestlei <zestlei@tencent.com>
2024-01-19 16:43:26 +0800

    feat:增加了贡献者名单

---
+++ lib/widgets/app_contributer.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class ContributorsPage extends StatelessWidget {
+  // 注意这里没有使用 const
+  ContributorsPage({Key? key}) : super(key: key);
+
+  final List<String> contributors = [
+    '蓝色的蘑菇头奇诺比奥',
+    // 在这里添加更多贡献者
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('贡献者名单'),
+      ),
+      body: ListView.builder(
+        itemCount: contributors.length,
+        itemBuilder: (BuildContext context, int index) {
+          return ListTile(
+            leading: const Icon(Icons.person),
+            title: Text(contributors[index]),
+          );
+        },
+      ),
+    );
+  }
+}
--- lib/widgets/app_darwer.dart
+++ lib/widgets/app_darwer.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:wows_kokomi_app/widgets/app_contributer.dart';
 import 'package:wows_kokomi_app/widgets/user_search_view.dart';
 
 class AppDrawer extends StatelessWidget {
@@ -30,6 +31,17 @@ class AppDrawer extends StatelessWidget {
               Navigator.push(
                 context,
                 MaterialPageRoute(builder: (context) => const UserInputPage()),
+              );
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.supervised_user_circle),
+            title: const Text('感谢名单'),
+            onTap: () {
+              // 在这里执行跳转到搜索界面的操作
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => ContributorsPage()),
               );
             },
           ),
