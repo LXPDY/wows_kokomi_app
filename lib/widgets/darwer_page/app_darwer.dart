6a230f5b1dc25252b1f08007b7656351c2e628da
zestlei <zestlei@tencent.com>
2024-01-22 14:13:45 +0800

    feat:优化工程结构

--- lib/widgets/darwer_page/app_darwer.dart
+++ lib/widgets/darwer_page/app_darwer.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:wows_kokomi_app/widgets/app_contributer.dart';
+import 'package:wows_kokomi_app/widgets/darwer_page/app_contributer.dart';
 import 'package:wows_kokomi_app/widgets/search_page/user_search_view.dart';
+
 
 class AppDrawer extends StatelessWidget {
   const AppDrawer({Key? key}) : super(key: key);
@@ -27,7 +28,7 @@ class AppDrawer extends StatelessWidget {
             leading: const Icon(Icons.search),
             title: const Text('搜索玩家'),
             onTap: () {
-              // 在这里执行跳转到搜索界面的操作
+              // 跳转到搜索界面的操作
               Navigator.push(
                 context,
                 MaterialPageRoute(builder: (context) => const UserInputPage()),
@@ -38,7 +39,7 @@ class AppDrawer extends StatelessWidget {
             leading: const Icon(Icons.supervised_user_circle),
             title: const Text('感谢名单'),
             onTap: () {
-              // 在这里执行跳转到搜索界面的操作
+              // 跳转到感谢名单
               Navigator.push(
                 context,
                 MaterialPageRoute(builder: (context) => ContributorsPage()),
