23b9d3487b1eb9fd8a78e620d55df87cf3994853
zestlei <zestlei@tencent.com>
2024-01-23 15:01:03 +0800

    feat:添加了返回主页的方法

--- lib/widgets/darwer_page/app_darwer.dart
+++ lib/widgets/darwer_page/app_darwer.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:wows_kokomi_app/common/user_global.dart';
+import 'package:wows_kokomi_app/models/server_name.dart';
 import 'package:wows_kokomi_app/widgets/darwer_page/app_contributer.dart';
+import 'package:wows_kokomi_app/widgets/main_page/app_home_page.dart';
 import 'package:wows_kokomi_app/widgets/search_page/user_search_view.dart';
 
 class AppDrawer extends StatelessWidget {
@@ -7,65 +10,98 @@ class AppDrawer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Drawer(
-      child: ListView(
-        padding: EdgeInsets.zero,
-        children: <Widget>[
-          const DrawerHeader(
-            decoration: BoxDecoration(
-              color: Colors.blue,
-            ),
-            child: Text(
-              'WOWS KOKOMI',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 24,
+    return FutureBuilder<String?>(
+      future: LocalStorage.getDefaultAccount(),
+      builder: (BuildContext context, AsyncSnapshot<String?> snapshot) {
+        String? userName;
+        String? aid;
+        ServerName? serverName;
+        if (snapshot.connectionState == ConnectionState.done) {
+          if (snapshot.data != null) {
+            final aidAndServerName = snapshot.data!.split(':');
+            aid = aidAndServerName[0];
+            userName = aidAndServerName[1];
+            serverName = ServerName.values
+                .firstWhere((element) => element.name == aidAndServerName[2]);
+          }
+        }
+        return Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: <Widget>[
+              GestureDetector(
+                onTap: userName != null
+                    ? () {
+                        // 跳转到绑定用户主页的操作
+                        Navigator.pushAndRemoveUntil(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => MyHomePage(
+                                initialIndex: 0,
+                                aid: aid.toString(),
+                                serverName: serverName!,
+                              ),
+                            ), (route) {
+                          return false;
+                        });
+                      }
+                    : null,
+                child: DrawerHeader(
+                  decoration: const BoxDecoration(
+                    color: Colors.blue,
+                  ),
+                  child: Text(
+                    userName ?? '未绑定',
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 24,
+                    ),
+                  ),
+                ),
               ),
-            ),
-          ),
-          ListTile(
-            leading: const Icon(Icons.search),
-            title: const Text('搜索玩家'),
-            onTap: () {
-              // 跳转到搜索界面的操作
-              Navigator.pushAndRemoveUntil(
-                context,
-                MaterialPageRoute(builder: (context) => const UserInputPage()),
-                (route) {
-                  // 保留类型为 UserInputPage 的页面，删除其他类型为 UserInputPage 的页面
-                  if (route.settings.name == '/') {
-                    return true;
-                  }
-                  if (route.settings.name == '/userInput') {
-                    return true;
-                  }
-                  return !route.hashCode.runtimeType
-                      .toString()
-                      .contains('UserInputPage');
+              ListTile(
+                leading: const Icon(Icons.search),
+                title: const Text('搜索玩家'),
+                onTap: () {
+                  Navigator.pushAndRemoveUntil(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const UserInputPage()),
+                    (route) {
+                      if (route.settings.name == '/') {
+                        return true;
+                      }
+                      if (route.settings.name == '/userInput') {
+                        return true;
+                      }
+                      return !route.hashCode.runtimeType
+                          .toString()
+                          .contains('UserInputPage');
+                    },
+                  );
                 },
-              );
-            },
+              ),
+              ListTile(
+                leading: const Icon(Icons.list),
+                title: const Text('账户列表'),
+                onTap: () {
+                  Navigator.pushNamed(context, '/accountList');
+                },
+              ),
+              ListTile(
+                leading: const Icon(Icons.supervised_user_circle),
+                title: const Text('感谢名单'),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => ContributorsPage()),
+                  );
+                },
+              ),
+            ],
           ),
-          ListTile(
-            leading: const Icon(Icons.list),
-            title: const Text('账户列表'),
-            onTap: () {
-              Navigator.pushNamed(context, '/accountList');
-            },
-          ),
-          ListTile(
-            leading: const Icon(Icons.supervised_user_circle),
-            title: const Text('感谢名单'),
-            onTap: () {
-              // 跳转到感谢名单
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => ContributorsPage()),
-              );
-            },
-          ),
-        ],
-      ),
+        );
+      },
     );
   }
 }
